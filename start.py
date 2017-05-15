2ffff7dddb8b724a163301c1dc178861c06abf7a
laixintao <laixintao1995@163.com>
2017-05-15 07:01:26 +0000

    fix global variable

--- start.py
+++ start.py
@@ -2,7 +2,6 @@
 
 from telegram.ext import Updater, CommandHandler
 
-total_seconds = 0
 
 import logging
 
@@ -11,6 +10,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                             level=logging.INFO)
 
 logger = logging.getLogger(__name__)
+
 
 def get_token():
     with open('token.txt') as token_file:
@@ -22,14 +22,18 @@ def start(bot, update):
     update.message.reply_text('请续命1s')
 
 def too_young_too_simple(bot, update):
+    global total_seconds
     update.message.reply_text(
         '{}为长者续了一秒，目前一共{}秒'.format(update.message.from_user.first_name, total_seconds))
     total_seconds += 1
 
-updater = Updater(get_token())
+if __name__ == '__main__':
+    global total_seconds
+    total_seconds = 0
+    updater = Updater(get_token())
 
-updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CommandHandler('too_young_too_simple', too_young_too_simple))
+    updater.dispatcher.add_handler(CommandHandler('start', start))
+    updater.dispatcher.add_handler(CommandHandler('too_young_too_simple', too_young_too_simple))
 
-updater.start_polling()
-updater.idle()
+    updater.start_polling()
+    updater.idle()
